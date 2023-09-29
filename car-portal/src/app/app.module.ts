e328f0f1db552f33e9f0826dc530992d0292b9b2
Akash Yadav <Akash.Yadav@asl.mum.sst>
2023-09-29 14:38:03 +0530

    rm landing page added

--- car-portal/src/app/app.module.ts
+++ car-portal/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RmHomepageComponent } from './rm-homepage/rm-homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RmHomepageComponent
   ],
   imports: [
     BrowserModule,
---
+++ car-portal/src/app/rm-homepage/rm-homepage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-rm-homepage',
+  templateUrl: './rm-homepage.component.html',
+  styleUrls: ['./rm-homepage.component.scss']
+})
+export class RmHomepageComponent {
+  location:any[]=["mumbai","delhi","goa","up","mp"]
+
+  //=======================================================
+}
