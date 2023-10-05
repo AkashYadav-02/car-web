21e84575dda55ed18b1469eb7d185979ab687757
Neeraj Kasar <neeraj.kasar@asl.mum.sst>
2023-10-05 17:34:45 +0530

    Nav bar chages

--- car-portal/src/app/navbar-after-home-page/navbar-after-home-page.component.ts
+++ car-portal/src/app/navbar-after-home-page/navbar-after-home-page.component.ts
@@ -1,20 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, HostListener} from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RedirectMenuService } from 'src/services/redirect-menu.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-navbar-after-home-page',
   templateUrl: './navbar-after-home-page.component.html',
   styleUrls: ['./navbar-after-home-page.component.scss']
 })
-export class NavbarAfterHomePageComponent {
+export class NavbarAfterHomePageComponent implements OnInit{
   isExpand=false;
+
+  @Input() navtheme?:string;
+
+  txtcolor='';
+  aurologo='';
+  bgcolor='';
+  like='';
+  noti='';
+  sett='';
+  sideburger=''
+
+  ngOnInit(): void {
+    if (this.navtheme =='blacktheme'){
+      this.txtcolor='white';
+      this.bgcolor='rgba(0, 0, 0, 0)'
+      this.aurologo='../../assets/Aurionpro.png';
+      this.like='../../assets/logos/Like.png';
+      this.noti='../../assets/logos/Notification.png';
+      this.sett='../../assets/logos/Settings.png';
+      this.sideburger='../../assets/logos/white-burger.png';
+    } else if (this.navtheme =='whitetheme'){
+      this.txtcolor='black';
+      this.bgcolor='white'
+      this.aurologo='../../assets/logos/logo.png';
+      this.like='../../assets/logos/Like2.png';
+      this.noti='../../assets/logos/Notification2.png';
+      this.sett='../../assets/logos/Settings2.png';
+      this.sideburger='../../assets/logos/black-burger.png';
+    }
+  }
 
   constructor(private router:Router,
     private fb: FormBuilder,
     private redirectMenu : RedirectMenuService,
      ) {}
+
+    //  @HostListener('window:scroll', ['$event']) 
+    // scrollHandler(event:Event) {
+    //   console.debug("Scroll Event");
+    //   this.navtheme='';
+    // }
 
   expandSideBar(){
 
--- car-portal/src/app/navbar/navbar.component.ts
+++ car-portal/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RedirectMenuService } from 'src/services/redirect-menu.service';
 
 @Component({
@@ -6,13 +6,33 @@ import { RedirectMenuService } from 'src/services/redirect-menu.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  @Input() navtheme?:string;
+
+  txtcolor='white';
+  aurologo='../../assets/Aurionpro.png';
+  bgcolor='rgba(0, 0, 0, 0)';
+
+  ngOnInit(): void {
+    if (this.navtheme =='blacktheme'){
+      this.txtcolor='white';
+      this.bgcolor='rgba(0, 0, 0, 0)'
+      this.aurologo='../../assets/Aurionpro.png';
+    } else if (this.navtheme =='whitetheme'){
+      this.txtcolor='black';
+      this.bgcolor='white'
+      this.aurologo='../../assets/logos/logo.png';
+    }
+  }
 
   constructor(
     private redirectService: RedirectMenuService
     ){
-
   }
+
+  
+
 
   redirect(path : string){
     this.redirectService.redirectTo(path);
