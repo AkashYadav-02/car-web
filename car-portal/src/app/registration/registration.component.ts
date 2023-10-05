bed52f631c10547b90b1d98767174a1e032291a1
Yash Shinde <Yash.Shinde@asl.mum.sst>
2023-10-05 23:40:44 +0530

    05-10-2023 night

--- car-portal/src/app/registration/registration.component.ts
+++ car-portal/src/app/registration/registration.component.ts
@@ -1,112 +1,64 @@
-import { Component, ViewChild } from '@angular/core';
-
-import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
-
-import { MessageService } from 'primeng/api';
-
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RedirectMenuService } from 'src/services/redirect-menu.service';
+
+function passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  if (password && confirmPassword && password.value !== confirmPassword.value) {
+    return { passwordMismatch: true };
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss'],
-  providers: [MessageService]
 })
 export class RegistrationComponent {
-  email:string =''
+  email: string = '';
+  fname: string = '';
+  lname: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  register: FormGroup | any;
 
-  fname:string = ''
-
-  lname: string='';
-
-  password : string='';
-
-  confirmPassword : string='';
-
-  register:FormGroup|any;
-
-  constructor(private router:Router,private fb: FormBuilder,
-    private redirectMenu : RedirectMenuService,
-    ) {}
-
-
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private redirectMenu: RedirectMenuService
+  ) {}
 
   ngOnInit(): void {
-
-
-
-    this.register = this.fb.group({
-
+    this.register = this.formBuilder.group({
       fname: ['', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z ]*')]],
-
-    lname: ['', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z ]*')]],
-
-        email: ['', [Validators.required, Validators.email]],
-
+      lname: ['', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z ]*')]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
-
       confirmPassword: ['', [Validators.required]],
-
-    termsAndConditions: [false, Validators.requiredTrue],
-
-})
-
-
-
-   }
-
-
-
-   registerdata(){
-
-    console.log(this.register.value);
-
+      termsAndConditions: [false, Validators.requiredTrue],
+    }, {
+      validators: passwordMatchValidator, // Validate password and confirmPassword
+    });
   }
 
-  redirect(path : string){
+  redirect(path: string) {
     this.redirectMenu.redirectTo(path);
   }
 
-
-
-redirectToForgotPassPage(){
-
-  this.router.navigate(['forgot']);
-
-}
-
-
-
-
-
-onSubmit() {
-
-  if (this.register && this.register.valid) {
-
-    console.log('Form submitted');
-
-    this.registerdata();
-
+  redirectToForgotPassPage() {
+    this.router.navigate(['forgot']);
   }
 
-   else {
-
-    console.log('Please fill in all required fields.');
-
+  onSubmit() {
+    if (this.register && this.register.valid) {
+      console.log('Form submitted');
+      // You can perform additional actions here, e.g., sending the form data to an API.
+    } else {
+      console.log('Please fill in all required fields and make sure passwords match.');
+    }
   }
-
-
-
 }
-
-
-
-}
-
-
-
-
-   
-
-
